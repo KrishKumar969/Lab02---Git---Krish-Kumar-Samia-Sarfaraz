234bdfbc48aaceb443a4e6a85021c466a0946eec
KrishKumar969 <kk09969@st.habib.edu.pk>
2026-02-12 11:05:15 +0500

    task2

---
+++ Lab4/task2.s
@@ -0,0 +1,32 @@
+.text
+.globl main
+main:
+    addi x10,x0,10
+    jal x1, sum  # jump to sum  
+    
+    # Print the result
+    mv x11, x10         # move result to x11 for print syscall
+    li x10, 1        
+    ecall               # print it
+    beq x0,x0,end
+    
+    sum:
+        addi sp , sp , -8 #adjust stack for 2 items
+        sw x1 , 4(sp)  #save return address
+        sw x10 , 0(sp) # save argument n
+        addi x5 , x10 , -1 # x5 = n - 1
+        bge x5 , x0 , L1 # if (n - 1) >= 0, go to L1
+        addi x10 , x0 , 0 # return 1
+        addi sp , sp , 8 # pop stack
+        jalr x0 , 0(x1) # return
+    L1:
+        addi x10 , x10 , -1 # argument = n - 1
+        jal x1 , fact # recursive call
+        addi x6 , x10 , 0 # save result of sum(n -1)
+        lw x10 , 0(sp) # restore original n
+        lw x1 , 4(sp) # restore return address
+        addi sp , sp , 8 # pop stack
+        add x10 , x10 , x6 # n + sum(n -1)
+        jalr x0 , 0(x1) # return
+end:
+    j end
