efa1cc3c6718b7e81c6d0faf841503aa5f7fed05
KrishKumar969 <kk09969@st.habib.edu.pk>
2026-03-18 19:25:08 +0500

    Update AddressDecoder.v

--- CALab8/CALab8.srcs/sources_1/new/AddressDecoder.v
+++ CALab8/CALab8.srcs/sources_1/new/AddressDecoder.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03/12/2026 09:50:22 AM
+// Create Date: 03/12/2026 09:50:50 AM
 // Design Name: 
-// Module Name: AddressDecoder
+// Module Name: DataMemory
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,16 +20,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module AddressDecoder(
+module DataMemory(
+    input clk,
+    input rst,
+    input MemWrite,
     input [9:0] address,
-    output DataMem,
-    output LEDWrite,
-    output SwitchReadEnable
+    input [31:0] WriteData,
+    output [31:0] ReadData
     );
     
-    wire [1:0] signal = address[9:8];
+    reg [31:0] Memory [511:0];
+    integer i;
     
-    assign DataMem          = (signal == 2'b00);
-    assign LEDWrite         = (signal == 2'b01);
-    assign SwitchReadEnable = (signal == 2'b10);
+    initial begin
+        for (i = 0; i < 512; i = i + 1)
+            Memory[i] = i;
+    end
+       
+    wire [7:0] location = address [7:0]; 
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            Memory[location] <= 32'd0;
+        else if (MemWrite)
+            Memory[location] <= WriteData;
+    end    
+    
+    assign ReadData = Memory[location]; 
 endmodule
