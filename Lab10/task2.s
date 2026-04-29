efde7ace761015d12f3a1715fb67212328fc2ec5
KrishKumar969 <kk09969@st.habib.edu.pk>
2026-04-30 04:59:10 +0500

    Update task2.s

--- Lab10/task2.s
+++ Lab10/task2.s
@@ -1,38 +1,58 @@
 .text
 .globl _start
+
 _start:
-    lui  x2,  0x00000
-    addi x2,  x2,  0x1FC
+    addi x2,  x0,  0x1FC
     addi x5,  x0,  512
     addi x6,  x0,  768
     addi x7,  x0,  772
-    lui  x18, 0x3
-    addi x18, x18, 0x30D5 & 0xFFF
+
+    addi x18, x0,  0
+    addi x18, x18, 2047
+    addi x18, x18, 2047
+    addi x18, x18, 2047
+    addi x18, x18, 2047
+    addi x18, x18, 2047
+    addi x18, x18, 2047
+    addi x18, x18, 219
+
     addi x8,  x0,  0
     sw   x0,  0(x5)
+
     j    fsm_loop
 
 fsm_loop:
     lw   x20, 0(x7)
-    bne  x20, x0,  do_reset
+    beq  x20, x0, no_reset_main
+    j    do_reset
+
+no_reset_main:
     lw   x9,  0(x6)
-    beq  x9,  x0,  fsm_loop
+    beq  x9,  x0, fsm_loop
+
     addi x8,  x0,  1
     sw   x9,  0(x5)
+
     addi x10, x9,  0
+
     addi x2,  x2,  -4
     sw   x1,  0(x2)
+
     jal  x1,  countdown
+
     lw   x1,  0(x2)
     addi x2,  x2,  4
+
     addi x8,  x0,  0
     sw   x0,  0(x5)
+
     j    fsm_loop
 
 do_reset:
     addi x8,  x0,  0
     addi x9,  x0,  0
     sw   x0,  0(x5)
+
     j    fsm_loop
 
 countdown:
@@ -42,13 +62,21 @@ countdown:
 
 countdown_loop:
     lw   x20, 0(x7)
-    bne  x20, x0,  countdown_reset
-    beq  x10, x0,  countdown_done
+    beq  x20, x0, no_reset_countdown
+    j    countdown_reset
+
+no_reset_countdown:
+    beq  x10, x0, countdown_done
     sw   x10, 0(x5)
+
     addi x11, x18, 0
+
 delay_loop:
     addi x11, x11, -1
-    bne  x11, x0,  delay_loop
+    beq  x11, x0, delay_done
+    j    delay_loop
+
+delay_done:
     addi x10, x10, -1
     j    countdown_loop
 
@@ -56,12 +84,16 @@ countdown_reset:
     lw   x8,  0(x2)
     lw   x1,  4(x2)
     addi x2,  x2,  8
+    addi x2,  x2,  4
+
     sw   x0,  0(x5)
     addi x8,  x0,  0
+
     j    fsm_loop
 
 countdown_done:
     lw   x8,  0(x2)
     lw   x1,  4(x2)
     addi x2,  x2,  8
+
     jalr x0,  x1,  0
